56ad06dd07b576a5fdaea8ac3b5b812433de9f67
san-est <vasil.boiadzhiev@gmail.com>
2024-06-15 16:41:48 +0300

    Testing

--- terraform/3-main.tf
+++ terraform/3-main.tf
@@ -1,5 +1,16 @@
 data "google_client_config" "default" {}
 
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
+
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
