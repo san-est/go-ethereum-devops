fdbaa9c33190d08a3a74eafd8ae4a2c61fede5bf
san-est <vasil.boiadzhiev@gmail.com>
2024-06-15 14:15:21 +0300

    Testing.

--- terraform/go-eth-app.tf
+++ terraform/go-eth-app.tf
@@ -39,9 +39,6 @@ provider "kubernetes" {
   host                   = "https://${google_container_cluster.primary.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-
-  # Enforce dependency on cluster creation
-  depends_on = [google_container_cluster.primary]
 }
 
 resource "kubernetes_namespace" "default" {
